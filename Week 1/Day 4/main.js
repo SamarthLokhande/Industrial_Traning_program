1fa169530792fdbbcd5a20f263559ac2b9089988
SamarthLokhande <samlokhande05@gmail.com>
2022-07-07 16:27:03 +0530

    login page in html

--- Week 1/Day 4/main.js
+++ Week 1/Day 4/main.js
@@ -2,5 +2,19 @@
             function fun(){
             alert("external js")
         }
+let username
+let password
+
+function get(){
+   username=document.getElementById("username").value;
+    password=document.getElementById("password").value;
+
+    if((username=="test") && (password=="test1") ){
+        alert("login successfully ")
+    }
+    else
+       {
+        alert("login failed")}
+}
         
     
