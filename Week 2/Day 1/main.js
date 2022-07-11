2a849ba543eb080b2f9eda851a1c7a9d7e56256a
SamarthLokhande <samlokhande05@gmail.com>
2022-07-11 16:54:24 +0530

    create account

---
+++ Week 2/Day 1/main.js
@@ -0,0 +1,12 @@
+function create(){
+
+
+let Firstname = document.getElementById("Firstname").value;
+let Lastname = document.getElementById("Lastname").value;
+let email = document.getElementById("email").value;
+let Phone no. = document.getElementById("Phone no.").value;
+let clgname = document.getElementById("clgname").value;
+
+document.write(Firstname + "<br/>"+Lastname + "<br/>" + email + " <br/>" + Phone no. + " <br/>" +clgname);
+}
+
